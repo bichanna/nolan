90334303fedc6b334e4ca8119abf8eca804af671
bichanna <nobu.bichanna@gmail.com>
2024-04-03 20:28:12 -0400

    Update something in parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -11,15 +11,29 @@ use crate::types::Type;
 
 pub mod ast;
 
+type ParseResult<T> = Result<T, ParseError>;
+
+macro_rules! throw_error {
+    ($span: expr, $msg: expr, $($f: expr),*) => {
+        return Err(Spanned(ParseErrorKind::ParseErr(format!($msg, $(f),*)), $span));
+    };
+
+    ($span: expr, $msg: expr) => {
+        return Err(Spanned(ParseErrorKind::ParseErr($msg.to_string())));
+    }
+}
+
 /// A recursive-descent parser that converts tokenized Nolan source code into an AST.
 struct Parser<'a> {
     source: SourcePath,
     lexer: Lexer<'a, Token>,
     ast: Vec<TopLevelExpr>,
     current: Result<Token, LexError>,
+    errors: Vec<ParseError>,
+    done: bool,
 }
 
-pub fn parse(source: SourcePath) -> Result<Module, ParseError> {
+pub fn parse(source: SourcePath) -> Result<Module, Vec<ParseError>> {
     let module_name = source
         .0
         .file_stem()
@@ -36,29 +50,70 @@ pub fn parse(source: SourcePath) -> Result<Module, ParseError> {
 
     if let Some(ref current) = current {
         if let Err(err) = current {
-            return Err(Spanned(
+            return Err(vec![Spanned(
                 ParseErrorKind::LexErr(err.clone()),
                 lexer.span(),
-            ));
+            )]);
         }
     } else {
         return Ok(Module::new(module_name, source));
     }
 
-    let current = current.unwrap();
-    let (source, exprs) =
-        Parser { source, lexer, ast: Vec::new(), current }.parse();
+    let (source, exprs) = Parser::new(source, lexer, current.unwrap()).parse();
 
     Ok(Module {
         name: module_name,
         path: source,
-        expressions: exprs,
+        expressions: exprs?,
         type_: Type::Void,
     })
 }
 
 impl<'a> Parser<'a> {
-    fn parse(self) -> (SourcePath, Vec<TopLevelExpr>) {
+    fn new(
+        source: SourcePath,
+        lexer: Lexer<'a, Token>,
+        current: Result<Token, LexError>,
+    ) -> Self {
+        Self {
+            source,
+            lexer,
+            current,
+            errors: Vec::new(),
+            ast: Vec::new(),
+            done: false,
+        }
+    }
+
+    fn parse(
+        mut self,
+    ) -> (SourcePath, Result<Vec<TopLevelExpr>, Vec<ParseError>>) {
+        while !self.done {
+            let result = self.parse_top_level_expression();
+            if let Err(err) = result {
+                self.errors.push(err);
+            } else {
+                self.ast.push(result.unwrap());
+            }
+        }
+
+        if self.errors.is_empty() {
+            (self.source, Ok(self.ast))
+        } else {
+            (self.source, Err(self.errors))
+        }
+    }
+
+    #[inline]
+    fn next(&mut self) {
+        if let Some(token) = self.lexer.next() {
+            self.current = token;
+        } else {
+            self.done = true;
+        }
+    }
+
+    fn parse_top_level_expression(&mut self) -> ParseResult<TopLevelExpr> {
         todo!()
     }
 }
