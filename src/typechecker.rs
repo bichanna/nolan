a73feca266b60a2337d7aa3fdf71b46390f75cbb
bichanna <nobu.bichanna@gmail.com>
2024-06-17 16:13:08 -0400

    Type checker skeleton

--- src/typechecker.rs
+++ src/typechecker.rs
@@ -111,7 +111,7 @@ impl<'a> TypeChecker<'a> {
     }
 
     fn check_func(&mut self) {
-        let TopLevelExpr::Func(func) = self.current().clone() else {
+        let TopLevelExpr::Func(mut func) = self.current().clone() else {
             unreachable!()
         };
 
@@ -119,7 +119,7 @@ impl<'a> TypeChecker<'a> {
             // ??
         }
 
-        self.check_closure(func.closure);
+        self.check_closure(&mut func.closure);
     }
 
     fn check_module(&mut self) {
@@ -217,21 +217,225 @@ impl<'a> TypeChecker<'a> {
         }
     }
 
-    fn check_expr(&mut self, expr: Expr) -> Option<Type> {
+    fn check_expr(&mut self, expr: &mut Expr) -> Option<Type> {
         match expr {
             Expr::Int(int_literal) => Some(int_literal.get_type().clone()),
             Expr::Float(float) => Some(float.get_type().clone()),
             Expr::Str(str_literal) => Some(str_literal.get_type().clone()),
             Expr::Bool(bool_literal) => Some(bool_literal.get_type().clone()),
-            Expr::List(list) => self.check_list(*list),
-            Expr::Tuple(tup) => self.check_tuple(*tup),
-            _ => None,
+            Expr::Void(void_literal) => Some(void_literal.get_type().clone()),
+            Expr::List(list) => {
+                list.type_ = self.check_list(list)?;
+                Some(list.type_.clone())
+            }
+            Expr::Tuple(tup) => {
+                tup.type_ = self.check_tuple(tup)?;
+                Some(tup.type_.clone())
+            }
+            Expr::Ident(ident) => {
+                ident.type_ = self.check_ident(ident)?;
+                Some(ident.type_.clone())
+            }
+            Expr::EnumVarAccess(access) => {
+                access.type_ = self.check_enum_var_access(access)?;
+                Some(access.type_.clone())
+            }
+            Expr::StructFieldAccess(access) => {
+                access.type_ = self.check_struct_field_access(access)?;
+                Some(access.type_.clone())
+            }
+            Expr::ModAccess(access) => {
+                access.type_ = self.check_mod_access(access)?;
+                Some(access.type_.clone())
+            }
+            Expr::Call(call) => {
+                call.type_ = self.check_call(call)?;
+                Some(call.type_.clone())
+            }
+            Expr::StructInit(init) => {
+                init.type_ = self.check_struct_init(init)?;
+                Some(init.type_.clone())
+            }
+            Expr::Closure(closure) => {
+                closure.type_ = self.check_closure(closure)?;
+                Some(closure.type_.clone())
+            }
+            Expr::Binary(binary) => {
+                binary.type_ = self.check_binary(binary)?;
+                Some(binary.type_.clone())
+            }
+            Expr::Unary(unary) => {
+                unary.type_ = self.check_unary(unary)?;
+                Some(unary.type_.clone())
+            }
+            Expr::Group(group) => self.check_expr(&mut group.expression),
+            Expr::If(i) => {
+                i.type_ = self.check_if(i)?;
+                Some(i.type_.clone())
+            }
+            Expr::When(when) => {
+                when.type_ = self.check_when(when)?;
+                Some(when.type_.clone())
+            }
+            Expr::AssignVar(assign) => {
+                assign.type_ = self.check_assign_var(assign)?;
+                Some(assign.type_.clone())
+            }
+            Expr::DefVar(def_var) => {
+                def_var.type_ = self.check_def_var(def_var)?;
+                Some(def_var.type_.clone())
+            }
+            Expr::Index(index) => {
+                index.type_ = self.check_index(index)?;
+                Some(index.type_.clone())
+            }
+            Expr::While(whle) => {
+                whle.type_ = self.check_while(whle)?;
+                Some(whle.type_.clone())
+            }
+            Expr::Break(brk) => Some(brk.type_.clone()),
+            Expr::Return(rtn) => {
+                self.check_return(rtn)?;
+                Some(rtn.get_type().clone())
+            }
+            Expr::Match(mtch) => {
+                mtch.type_ = self.check_match(mtch)?;
+                Some(mtch.type_.clone())
+            }
+        }
+    }
+
+    fn check_match(
+        &mut self,
+        Match { expression, expressions, span, type_ }: &mut Match,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_return(
+        &mut self,
+        Return { value, span }: &mut Return,
+    ) -> Option<()> {
+        None
+    }
+
+    fn check_while(
+        &mut self,
+        While { condition, body, span, type_ }: &mut While,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_index(
+        &mut self,
+        Index { source, index, span, type_ }: &mut Index,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_def_var(
+        &mut self,
+        DefVar { name, value, span, type_ }: &mut DefVar,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_assign_var(
+        &mut self,
+        AssignVar { left, value, span, type_ }: &mut AssignVar,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_when(
+        &mut self,
+        When { condition, then, else_, span, type_ }: &mut When,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_if(
+        &mut self,
+        If { condition, then, else_, span, type_ }: &mut If,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_unary(
+        &mut self,
+        Unary { rhs, operator, span, type_ }: &mut Unary,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_binary(
+        &mut self,
+        Binary { lhs, rhs, operator, span, type_ }: &mut Binary,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_struct_init(
+        &mut self,
+        StructInit { name, arguments, span, type_ }: &mut StructInit,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_call(
+        &mut self,
+        Call { callee, arguments, span, type_ }: &mut Call,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_mod_access(
+        &mut self,
+        ModAccess { module, constant, span, type_ }: &mut ModAccess,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_struct_field_access(
+        &mut self,
+        StructFieldAccess { source, field, span, type_ }: &mut StructFieldAccess,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_enum_var_access(
+        &mut self,
+        EnumVarAccess { source, variant, span, type_ }: &mut EnumVarAccess,
+    ) -> Option<Type> {
+        None
+    }
+
+    fn check_ident(
+        &mut self,
+        Ident { name, span, type_: _ }: &mut Ident,
+    ) -> Option<Type> {
+        if self.type_env.has_type(name) {
+            let Either::Left(type_) = self.type_env.types.get(name).unwrap()
+            else {
+                self.errors.push(Spanned(
+                    format!(
+                        "{} does not have type",
+                        self.type_env.force_resolve(*name)
+                    ),
+                    span.clone(),
+                ));
+                return None;
+            };
+
+            Some(type_.clone())
+        } else {
+            None
         }
     }
 
     fn check_tuple(
         &mut self,
-        Tuple { values, span: _, type_ }: Tuple,
+        Tuple { values, span: _, type_ }: &mut Tuple,
     ) -> Option<Type> {
         let Type::Tup(_, ref types) = type_ else {
             self.errors.push(Spanned(
@@ -244,7 +448,7 @@ impl<'a> TypeChecker<'a> {
             return None;
         };
 
-        for (value, tt) in values.into_iter().zip(types) {
+        for (value, tt) in values.iter_mut().zip(types) {
             let t = self.check_expr(value)?;
             if !t.ignore_span_equal(tt) {
                 self.errors.push(Spanned(
@@ -259,16 +463,16 @@ impl<'a> TypeChecker<'a> {
             }
         }
 
-        Some(type_)
+        Some(type_.clone())
     }
 
     fn check_list(
         &mut self,
-        List { elements, span, type_: _ }: List,
+        List { elements, span, type_: _ }: &mut List,
     ) -> Option<Type> {
         if !elements.is_empty() {
             let elems = elements
-                .into_iter()
+                .iter_mut()
                 .map(|elem| self.check_expr(elem))
                 .collect::<Vec<Option<Type>>>();
 
@@ -297,24 +501,27 @@ impl<'a> TypeChecker<'a> {
                 }
             }
 
-            Some(Type::List(span, Box::new(first.clone())))
+            Some(Type::List(span.clone(), Box::new(first.clone())))
         } else {
-            Some(Type::List(span.clone(), Box::new(Type::Unknown(span))))
+            Some(Type::List(
+                span.clone(),
+                Box::new(Type::Unknown(span.clone())),
+            ))
         }
     }
 
     fn check_closure(
         &mut self,
-        Closure { parameters, return_type, mut body, span, type_: _ }: Closure,
+        Closure { parameters, return_type, ref mut body, span, type_: _ }: &mut Closure,
     ) -> Option<Type> {
-        for func_param in &parameters {
+        for func_param in &mut *parameters {
             self.check_type(&func_param.type_);
         }
 
-        self.check_type(&return_type);
+        self.check_type(return_type);
 
         let closure_type = Type::Func(
-            span,
+            span.clone(),
             parameters.iter().map(|param| param.type_.clone()).collect(),
             return_type.clone().into(),
         );
@@ -335,8 +542,10 @@ impl<'a> TypeChecker<'a> {
             }
 
             _ => {
-                let last_expr = body.pop().unwrap();
-                if !self.check_expr(last_expr)?.ignore_span_equal(&return_type)
+                let mut last_expr = body.pop().unwrap();
+                if !self
+                    .check_expr(&mut last_expr)?
+                    .ignore_span_equal(return_type)
                 {
                     return None;
                 }
